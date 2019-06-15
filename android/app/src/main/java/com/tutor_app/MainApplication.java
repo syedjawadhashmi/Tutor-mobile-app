1ecb644c9ffc6815269db5a256dcbc4b05846cb1
MubahirAli <mubahir22ali@gmail.com>
2019-06-15 22:05:47 +0500

    google done

--- android/app/src/main/java/com/tutor_app/MainApplication.java
+++ android/app/src/main/java/com/tutor_app/MainApplication.java
@@ -3,6 +3,7 @@ package com.tutor_app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGoogleSigninPackage(),
             new VectorIconsPackage(),
             new LinearGradientPackage(),
             new RNGestureHandlerPackage()
--- src/containers/LoginHoc/index.js
+++ src/containers/LoginHoc/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 // import { LoginManager } from 'react-native-fbsdk';
-// import { GoogleSignin, statusCodes } from 'react-native-google-signin';
+import { GoogleSignin, statusCodes } from 'react-native-google-signin';
 import { connect } from 'react-redux';
 
 import * as a from '../../redux/actions/authActions'
@@ -26,16 +26,16 @@ const LoginHOC = (WrappedComponent) => {
                     drawerLockMode: 'locked-closed'
                 }
                 componentDidMount() {
-                    // GoogleSignin.configure({
-                    //     scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
-                    //     webClientId: '238002584387-ep6jp147h9je00bv2cdrof9b6kv8in1f.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
-                    //     offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
-                    //     hostedDomain: '', // specifies a hosted domain restriction
-                    //     loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
-                    //     forceConsentPrompt: true, // [Android] if you want to show the authorization prompt at each login.
-                    //     accountName: '', // [Android] specifies an account name on the device that should be used
-                    //     iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
-                    // });
+                    GoogleSignin.configure({
+                        scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+                        webClientId: '238002584387-ep6jp147h9je00bv2cdrof9b6kv8in1f.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+                        offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+                        hostedDomain: '', // specifies a hosted domain restriction
+                        loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
+                        forceConsentPrompt: true, // [Android] if you want to show the authorization prompt at each login.
+                        accountName: '', // [Android] specifies an account name on the device that should be used
+                        iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+                    });
                 }
 
                 onChange = (name, value) => {
@@ -65,22 +65,24 @@ const LoginHOC = (WrappedComponent) => {
 
                 //  google signin
                 handleGoogleLogin = async () => {
-                    // try {
-                    //     await GoogleSignin.hasPlayServices();
-                    //     const userInfo = await GoogleSignin.signIn();
-                    //     this.setState({ userInfo });
-                    //     console.log("==>user", userInfo)
-                    // } catch (error) {
-                    //     if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-                    //         // user cancelled the login flow
-                    //     } else if (error.code === statusCodes.IN_PROGRESS) {
-                    //         // operation (f.e. sign in) is in progress already
-                    //     } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-                    //         // play services not available or outdated
-                    //     } else {
-                    //         // some other error happened
-                    //     }
-                    // }
+                    try {
+                        await GoogleSignin.hasPlayServices();
+                        const userInfo = await GoogleSignin.signIn();
+                        this.setState({ userInfo });
+                        alert('loginsucccess')
+                        console.log("==>user", userInfo)
+                    } catch (error) {
+                        if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+                            // user cancelled the login flow
+                        } else if (error.code === statusCodes.IN_PROGRESS) {
+                            // operation (f.e. sign in) is in progress already
+                        } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+                            // play services not available or outdated
+                        } else {
+                            // some other error happened
+                            // alert('some other error happened',error)
+                        }
+                    }
                 };
 
                 // facebooklogin
